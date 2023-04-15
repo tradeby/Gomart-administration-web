c74e8e21d25a3db1b0eba2684a2a5e962b4292d2
musa jahun <msjahun@live.com>
2023-04-15 16:29:33 +0100

    added buttons, to dashboard

--- src/features/dashboard/dashboard-graph-large.tsx
+++ src/features/dashboard/dashboard-graph-large.tsx
@@ -1,59 +1,153 @@
-import React from "react";
+import React, {useState} from "react";
 import {Chart} from "react-google-charts";
 import Button from "@atlaskit/button/standard-button";
 import Lozenge from "@atlaskit/lozenge";
+import './card-flip.css';
 
 export function UsersRegistrationGraph() {
+    const [isFlipped, setIsFlipped] = useState(false); // State to track the flip status
+    const handleFlipClick = () => {
+        setIsFlipped(!isFlipped);
+    };
     const options = {
         vAxis: {minValue: 0},
         legend: 'none',
+        animation: {
+            startup: true,
+            duration: 800,
+            easing: 'in'
+        },
         chartArea: {
-            top:10,
+            top: 10,
             left: 70,
             right: 30,
-            bottom:50
+            bottom: 50
+        }
+    };
+    const options2 = {
+        vAxis: {minValue: 0,},
+        hAxis: {
+            gridlines: { color: 'transparent' },// Change hAxis label color to blue
+        },
+        legend: 'none', // Set the background color
+        series: {
+            0: { color: '#00BDAE'}, // Change color of series 0 (first series) to blue
+        },
+        animation: {
+            startup: true,
+            duration: 800,
+            easing: 'in'
+        },
+        chartArea: {
+            top: 10,
+            left: 70,
+            right: 30,
+            bottom: 50
         }
     };
 
-    const data = [
-        ['Year', 'Sales', ],
-        ['Jan',  15000,  ],
-        ['Feb',  12500,    ],
-        ['Mar',  18000,     ],
-        ['Apr',  21000,      ],
-        ['May',  26000,      ],
-        ['Jun',  36000,      ],
-        ['Jul',  16000,      ],
+    const dataRevenue = [
+        ['Year', 'Sales',],
+        ['Jan', 15000,],
+        ['Feb', 12500,],
+        ['Mar', 18000,],
+        ['Apr', 21000,],
+        ['May', 26000,],
+        ['Jun', 36000,],
+        ['Jul', 16000,],
+
+    ];
+    const dataUsers = [
+        ['Year', 'users',],
+        ['Jan', 21,],
+        ['Feb', 50,],
+        ['Mar', 8,],
+        ['Apr', 30,],
+        ['May', 32,],
+        ['Jun', 26,],
+        ['Jul', 2,],
 
     ];
 
-    return <div className="bg-white shadow col-span-2 rounded-md h-96 w-full p-2 pb-0 pt-1">
-        <div className="flex space-x-4 p-5">
-            <div className="flex-1 space-y-6 py-1">
-                <div className="space-y-3">
-                    <div className="grid grid-cols-3 gap-1">
-                        <div className=" col-span-3 text-base accent-neutral-600 font-medium">{'Users Statistics'} <Lozenge
-                            appearance="new">New!</Lozenge></div>
-                        <div
-                            className="text-xs col-span-4  text-gray-400 font-medium">{'A simple graph showing flow of new users'}</div>
+    return <>
+        <div className='col-span-2  h-96 w-full  pb-0 ' style={{perspective: '1000px'}}>
 
+            <div className='col-span-2  h-96 w-full  pb-0 ' style={{
+                position:'relative',
+                transform: isFlipped ? 'rotateY(180deg)' : 'rotateY(0deg)',
+                transition: 'transform 0.5s ease-in-out',
+                transformStyle: 'preserve-3d',}}>
+
+            <div className=" flip-card-front  bg-white shadow rounded-md p-1" >
+                <div className="flex space-x-4 p-4">
+                    <div className="flex-1 space-y-6 py-1">
+                        <div className="space-y-3">
+                            <div className="grid grid-cols-3 gap-1">
+                                <div
+                                    className=" col-span-3 text-base accent-neutral-600 font-medium">{'Users Statistics'}
+
+                                    <Lozenge
+                                        appearance="new">New!</Lozenge>
+                                    <span className="pl-2"> <Button spacing="compact" >Week</Button>  </span>
+                                    <span className="pl-0"> <Button spacing="compact" >Months</Button>  </span>
+                                </div>
+                                <div
+                                    className="text-xs col-span-4  text-gray-400 font-medium">{'A simple graph showing flow of new users'}</div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-row gap-2">
+                        <Button isDisabled  > Users</Button>
+                        <Button appearance={'primary'} onClick={handleFlipClick}> Revenue</Button>
                     </div>
                 </div>
+                <div className="w-full bg-slate-100">
+                    <Chart
+                        chartType="AreaChart"
+                        data={dataUsers}
+                        options={options}
+                        height="18rem"
+                        width='100%'
+                        legendToggle
+                    />
+                </div>
             </div>
-            <div className="flex flex-row gap-2">
-                <Button appearance={'primary'}> Users</Button>
-                <Button> Revenue</Button>
+
+            <div className="flip-card-back bg-white  shadow rounded-md p-1"  >
+                <div className="flex space-x-4 p-4">
+                    <div className="flex-1 space-y-6 py-1">
+                        <div className="space-y-3">
+                            <div className="grid grid-cols-3 gap-1">
+                                <div
+                                    className=" col-span-3 text-base accent-neutral-600 font-medium ">{'Ads revenue'}
+                                    <span className="pl-2"> <Button spacing="compact" > Week</Button>  </span>
+                                    <span className="pl-0"> <Button spacing="compact" >Month</Button>  </span>
+
+                                </div>
+                                <div
+                                    className="text-xs col-span-4  text-gray-400 font-medium">{'A simple graph showing flow of new users'}</div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-row gap-2">
+                        <Button appearance={'primary'} onClick={handleFlipClick} > Users</Button>
+                        <Button isDisabled> Revenue</Button>
+                    </div>
+                </div>
+                <div className="w-full bg-slate-100">
+                    <Chart
+                        chartType="AreaChart"
+                        data={dataRevenue}
+                        options={options2}
+                        height="18rem"
+                        width='100%'
+                        legendToggle
+                    />
+                </div>
             </div>
         </div>
-        <div className="w-full bg-slate-100">
-            <Chart
-                chartType="AreaChart"
-                data={data}
-                options={options}
-                height="18rem"
-                width='100%'
-                legendToggle
-            />
         </div>
-    </div>;
+    </>;
 }
--- src/features/dashboard/dashboard-graph-mini.tsx
+++ src/features/dashboard/dashboard-graph-mini.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import {Chart} from "react-google-charts";
 import Button from "@atlaskit/button/standard-button";
+import {useNavigate} from "react-router-dom";
 
 export function UsersVsBusinessesGraph() {
+    const navigate = useNavigate();
     const options = {
         legend: "none",
+        animation:{
+            startup:true,
+            duration: 400,
+            easing: 'out',
+        },
+        vAxis: {
+            minValue: 0,
+            gridlines: { color: 'transparent' }, // Set vAxis gridlines color to transparent
+        },
         chartArea: {
             top:20,
             left: 50,
@@ -23,9 +34,9 @@ export function UsersVsBusinessesGraph() {
             <div className="flex-1 space-y-6 py-1">
                 <div className="space-y-3">
                     <div className="grid grid-cols-3 gap-1">
-                        <div className=" col-span-3 text-base accent-neutral-600 font-medium">{'Recent Activity'}</div>
+                        <div className=" col-span-3 text-base accent-neutral-600 font-medium">{'Users vs Businesses'}</div>
                         <div
-                            className="text-xs col-span-4  text-gray-400 font-medium">{'A view of all recent activity by users'}</div>
+                            className="text-xs col-span-4  text-gray-400 font-medium">{'A view that show users vs businesses distribution'}</div>
 
                     </div>
                 </div>
@@ -38,6 +49,23 @@ export function UsersVsBusinessesGraph() {
                 data={data}
                 options={options}
                 height="18rem"
+                chartEvents={[
+                    {
+                        eventName: 'select',
+                        callback: ({ chartWrapper }) => {
+                            // Get the selected column data
+                            const selectedItem = chartWrapper.getChart().getSelection()[0];
+                            if (selectedItem) {
+                                // Call your custom click handler
+                              if(data[selectedItem.row + 1][0] ==='Users'){
+                                  navigate('/users')
+                              }else{
+                                  navigate('/businesses/power-search')
+                              }
+                            }
+                        },
+                    },
+                ]}
                 legendToggle
             />
         </div>
